a082e558f25be7cd91884804d9a28f6cd306b672
shayan-saif <shayansaif@hotmail.com>
2021-11-13 21:15:05 -0500

    Added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProfileformComponent } from './profileform/profileform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
